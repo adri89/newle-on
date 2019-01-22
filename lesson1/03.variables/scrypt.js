652e47b3d66f56f17d5035f79c9b9ba538b6c3d9
adri89 <46706192+adri89@users.noreply.github.com>
2019-01-22 09:19:59 +0100

    deuxieme cours

---
+++ lesson1/03.variables/scrypt.js
@@ -0,0 +1,39 @@
+'use strict';
+let user; // déclare user variable
+let admin; // déclare admin variable
+
+user = 'sohpie'; //assigenr une valeur à une variable
+admin = user; // copiez la valeur d'user dans admin
+console.log('admin') //loggez la valeur d'admin dans la console
+
+user = 'mathieu' // réassigner la valeur d'une variable 
+
+console.log(user)
+console.log(admin)
+
+//  typage faible ou fort?
+
+let distance = '12 km'
+distance = 12;
+console.log(distance);
+
+// var, let ou const?
+
+let username = 'claire';
+const userRole =1;
+username = 'Emmanuel';
+console.log('alrighty', username)
+
+userRole = 2;
+console.log('userRole', userRole)
+
+const userAge;
+const Pi;
+let  navLinks;
+const earthWeight;
+const currentYear;
+const datepicker;
+const policePhoneNumber;
+let userWeight; 
+
+
---
+++ lesson1/04.data-types/scrypt.js
@@ -0,0 +1,35 @@
+let toto = "machin"
+let popo = 42
+console.log(popo)
+let myfloat = 1.1;
+console.log()
+let coco = true,
+console.log()
+let myString = undefined;
+console.log('myString', myString)
+let nono = null; 
+console.log(nomber('1'));
+
+let myString = '1'
+console.log(parseFloat(myString));
+
+console.log(+myString)
+
+let myString = 'hello'
+console.log(parseFloat(myString))
+
+let myString = '0'
+console.log(parseFloat(myString))
+
+//transformer une chaine de caractère en nombre
+console.log(Number(yourStringhere))
+console.log(pareInt(yourStringHere))
+console.log(+yourStringhere)
+
+//transformer chaine de caractère en boolean
+console.log(Boolean(ypurStringHere))
+
+//transformer un nombre en chaine de nombre
+let count = 10;
+console.log(count.toString())
+console.log(new Number(10))
